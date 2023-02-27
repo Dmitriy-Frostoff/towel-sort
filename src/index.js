54482c0004f56a31c8485f19eaf8b1d0205f493c
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-02-27 03:31:44 +0300

    fix: fix problems with functions on test running; task is solved

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+"use strict";
 
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+    const isArgumentsListEmpty = () => {
+      if ([...arguments].length === 0) {
+          return true;
+      }
+    }
+
+    if (isArgumentsListEmpty()) {
+        return [];
+      }
+
     const matrixCopy = matrix.map(subArray => subArray.slice());
-  
+    
     const isMatrixEmpty = (matrixCopy) => {
         if (matrixCopy.length === 0) {
-            return false;
+            return true;
         }
     }
-  
+
+    if (isMatrixEmpty(matrixCopy)) {
+        return matrixCopy;
+      }
+    
     const reverseOddRows = (element, index, matrixCopy) => {
         if (index % 2 !== 0) {
           return matrixCopy[index].reverse();
@@ -23,10 +38,7 @@ module.exports = function towelSort (matrix) {
             }
         }
       }
-      if (!isMatrixEmpty(matrixCopy)) {
-        return matrixCopy;
-      }
-      
+  
       matrixCopy.map(reverseOddRows);
   
       const simpleArray = [];
