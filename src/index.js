3d47601c4c106283c7c6371b08bee15c6be20e54
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-09-05 19:59:13 +0300

    refactor(towelSort function): simplify and clarify the logic, add JSDoc commenting

--- src/index.js
+++ src/index.js
@@ -1,45 +1,103 @@
-module.exports = function towelSort(matrix) {
-  const isArgumentsListEmpty = () => {
-    if ([...arguments].length === 0) {
-      return true;
-    }
-  };
+/**
+ * Reverse all the odd rows of the given @param{matrix} and return it transformed into flat array.
+ * If not a matrix was produced or an empty one, the empty flat array will be returned.
+ *
+ * @param {number[][] | undefined} matrix - 2d array(matrix) or undefined
+ *
+ * @returns {number[]} - flat array with odd matrix' rows reversed or empty array
+ *
+ * @example
+ *   const matrix = [
+ *    [1, 2, 3],
+ *    [4, 5, 6],
+ *    [7, 8, 9],
+ *   ];
+ *
+ *  towelSort(matrix) =>
+ *  OUTPUT:
+ *  [1, 2, 3, 6, 5, 4, 7, 8, 9]
+ *
+ *  const matrix = [
+ *    [1, 2, 4],
+ *    [5, 6, 7, 8],
+ *    [9, 12],
+ *  ];
+ *
+ *  towelSort(matrix) =>
+ *  OUTPUT:
+ *  [1, 2, 4, 8, 7, 6, 5, 9, 12]
+ *
+ *  const matrix = [];
+ *
+ *  towelSort(matrix) =>
+ *  OUTPUT:
+ *  []
+ *
+ *  towelSort() =>
+ *  OUTPUT:
+ *  []
+ *
+ */
+function towelSort(matrix) {
+  // utils
 
-  if (isArgumentsListEmpty()) {
+  /**
+   * Utility function for reversing odd rows of the 2d array (matrix)
+   *
+   * @param {number[]} element - current row
+   * @param {number} index - index of the row
+   * @param {number[][]} matrixArr - the entire produced 2d array
+   *
+   * @returns {number[]} - if current row is of odd index OUTPUT: reversed row
+   *  otherwise OUTPUT: row as is
+   *
+   * @example
+   *    const arr = [
+   *       [1, 2],
+   *       [3, 4],
+   *       [5, 6],
+   *     ]
+   *    arr.map(reverseOddRows) =>
+   *    OUTPUT:
+   *    arr [
+   *       [1, 2],
+   *       [4, 3],
+   *       [5, 6],
+   *     ]
+   *
+   */
+  function reverseOddRows(element, index, matrixArr) {
+    if (index % 2 !== 0) {
+      return matrixArr[index].reverse();
+    }
+
+    return element;
+  }
+
+  // logic
+  // check that given arguments are not empty and @param{matrix} is @type{Array}
+  if (arguments.length === 0 || !Array.isArray(matrix)) {
     return [];
   }
 
-  const matrixCopy = matrix.map((subArray) => subArray.slice());
+  // create copy of @link{matrix} due to FP paradigm
+  /** @type{number[][]} */
+  let matrixCopy = matrix.map((subArray) => subArray.slice());
 
-  const isMatrixEmpty = (matrixCopy) => {
-    if (matrixCopy.length === 0) {
-      return true;
-    }
-  };
-
-  if (isMatrixEmpty(matrixCopy)) {
-    return matrixCopy;
+  // check that @link{matrixCopy} is not empty
+  if (matrixCopy.length === 0) {
+    return [];
   }
 
-  const reverseOddRows = (element, index, matrixCopy) => {
-    if (index % 2 !== 0) {
-      return matrixCopy[index].reverse();
-    }
-  };
+  // reverse odd matrix rows
+  /** @type{number[][]} */
+  matrixCopy = matrixCopy.map(reverseOddRows);
 
-  const convertMatrixToSimpleArray = (matrixCopy, simpleArray) => {
-    for (let i = 0; i < matrixCopy.length; i += 1) {
-      for (let j = 0; j < matrixCopy[i].length; j += 1) {
-        simpleArray.push(matrixCopy[i][j]);
-      }
-    }
-  };
-
-  matrixCopy.map(reverseOddRows);
-
-  const simpleArray = [];
-
-  convertMatrixToSimpleArray(matrixCopy, simpleArray);
+  // flat the @link{matrixCopy}
+  /** @type{number[]} */
+  const simpleArray = matrixCopy.flat();
 
   return simpleArray;
-};
+}
+
+module.exports = towelSort;
